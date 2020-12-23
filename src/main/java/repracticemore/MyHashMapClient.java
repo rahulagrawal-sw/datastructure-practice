f66974a09eeb56d54009156485e2a648ec416eb0
Rahul Agrawal <rahulagrawal.sw@gmail.com>
2020-12-23 12:33:50 +0530

    HashMap using LinkedList

---
+++ src/main/java/repracticemore/MyHashMapClient.java
@@ -0,0 +1,15 @@
+package repracticemore;
+
+public class MyHashMapClient {
+    public static void main(String[] args) {
+        HashMapImpl hashMap = new HashMapImpl(2);
+        hashMap.putIntoMap("PUNE", 10);
+        hashMap.putIntoMap("NAGAR", 20);
+        hashMap.putIntoMap("AHNAGAR", 20);
+        hashMap.putIntoMap("AURANGABAD", 20);
+        hashMap.putIntoMap("MUMBAI", 20);
+
+        hashMap.putIntoMap("AHNAGAR", 50);
+        HashMapImpl.printHashMap(hashMap);
+    }
+}
